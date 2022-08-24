289570cb329c04488089a0e2a5da31e0d68bfb4d
hyeonmi3 <cc146@daum.net>
2022-08-24 10:47:31 +0900

    Java 문제 풀이

--- JavaProblemSolving/src/kr/hs/dgsw/d220824/Step1_7.java
+++ JavaProblemSolving/src/kr/hs/dgsw/d220824/Step1_7.java
@@ -1,5 +1,15 @@
 package kr.hs.dgsw.d220824;
 
-public class Step1_7 {
+import java.util.Scanner;
 
+public class Step1_7 {
+	// 10869
+	// BaekJoon 1´Ü°è 7¹ø
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.printf("%d\n");
+	}
 }
