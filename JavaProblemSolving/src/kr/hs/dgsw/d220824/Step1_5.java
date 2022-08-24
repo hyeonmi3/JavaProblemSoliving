a8e73deadfe9903c07575983c770d29d56fd9200
hyeonmi3 <cc146@daum.net>
2022-08-24 10:47:15 +0900

    Java 문제 풀이

---
+++ JavaProblemSolving/src/kr/hs/dgsw/d220824/Step1_5.java
@@ -0,0 +1,15 @@
+package kr.hs.dgsw.d220824;
+
+import java.util.Scanner;
+
+public class Step1_5 {
+	// 10998
+	// BaekJoon 1´Ü°è 5¹ø
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println(a * b);
+	}
+}
---
+++ JavaProblemSolving/src/kr/hs/dgsw/d220824/Step1_6.java
@@ -0,0 +1,15 @@
+package kr.hs.dgsw.d220824;
+
+import java.util.Scanner;
+
+public class Step1_6 {
+	// 1008
+	// BaekJoon 1´Ü°è 6¹ø
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double a = sc.nextDouble();
+		double b = sc.nextDouble();
+
+		System.out.println(a / b);
+	}
+}
